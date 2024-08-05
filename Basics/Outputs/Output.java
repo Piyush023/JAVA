72f099416abc72fc587a82ff17e13cf194b5b55e
Piyush023 <piyush.khurana@rario.com>
2024-08-06 03:24:54 +0530

    1st Module Finish - Java Basic Finish.

--- Basics/Outputs/Output.java
+++ Basics/Outputs/Output.java
@@ -8,10 +8,14 @@ public class Output {
         System.out.println(y);
         System.out.println(x+y);
         System.out.println(s);
+        double d = 10.3;
+//        boolean b = (x = d);
+//        System.out.println(b);
 
 // Format Printing -
         double f = 3.1423141;
-        System.out.format("Value of f is %f \n", f);
+        System.out.format("Value of f is %.2f \n", f);
+
 // Escape Sequence -
         System.out.println("C:\\abc.txt\\xyz.txt");
     }
---
+++ Basics/Pratice_Ques/Last_Digit.java
@@ -0,0 +1,8 @@
+package Basics.Pratice_Ques;
+
+public class Last_Digit {
+    public static void main(String[] args) {
+        int a = 123;
+        System.out.println(a%10);
+    }
+}
