aa94cd49a0763e5481adff00b17d3c9667c6b3c3
Piyush023 <piyush.khurana@rario.com>
2024-09-16 04:34:41 +0530

    Arrays Started and Done till the Avg of Array.

---
+++ Arrays/Practice/Average.java
@@ -0,0 +1,23 @@
+package Arrays.Practice;
+
+import java.util.Scanner;
+
+public class Average {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0;i< arr.length;i++){
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(arr.length);
+            avg(arr);
+        }
+        public static void avg(int arr[]){
+            int sum = 0;
+            for (int j : arr) {
+                sum += j;
+            }
+            System.out.println("Avg is - "+sum/arr.length);
+        }
+    }
